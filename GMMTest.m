f228ef2b25238b98dfd7c9af6d972bec74c5f3e9
gpldecha <guillaume.dechambrier@epfl.ch>
2016-03-18 16:37:51 +0100

    first

---
+++ GMMTest.m
@@ -0,0 +1,31 @@
+%% Test features of the GMM class
+
+clear all;
+K = 4;
+
+Priors = [0.4 0.1 0.25 0.25];
+Mu = zeros(2,4);
+Mu(:,1) = [0 0]';
+Mu(:,2) = [2 2]';
+Mu(:,3) = [-3 5]';
+Mu(:,4) = [0 -4];
+Sigma = repmat(eye(2,2),[1 1 4]);
+
+clear gmm;
+gmm = GMM(Priors,Mu,Sigma);
+gmm.color = [1 0 0];
+
+
+%% Graphics;
+
+figure; hold on;
+gmm.draw(gca());
+grid on;
+axis equal;
+
+
+
+%% Test Sampling
+
+X = gmm.sample(500);
+plot(gca,X(:,1),X(:,2),'+b');
---
+++ TrainGMM/train_gmm.m
@@ -0,0 +1,7 @@
+function [ output_args ] = train_gmm( input_args )
+%TRAIN_GMM Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
